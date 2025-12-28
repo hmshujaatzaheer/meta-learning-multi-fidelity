2624e3a64ebd8f27b83064461a72560ec0cc957e
H M Shujaat Zaheer <50784529+hmshujaatzaheer@users.noreply.github.com>
2025-12-28 04:04:05 -0500

    Implement initial tests for CausalDiscovery
    Add initial unit tests for CausalMF functionality.

--- tests/test_causal_mf.py
+++ tests/test_causal_mf.py
@@ -1,1 +1,36 @@
+"""
+Unit tests for CausalMF
 
+To be implemented during PhD.
+"""
+
+import pytest
+from src.causal_mf import CausalDiscovery
+
+
+def test_causal_discovery_initialization():
+    """Test causal discovery initialization"""
+    causal_model = CausalDiscovery(algorithm='pc', alpha=0.05)
+    assert causal_model.algorithm == 'pc'
+    assert causal_model.alpha == 0.05
+
+
+def test_discover_structure():
+    """Test causal structure discovery"""
+    # To be implemented
+    # Will test PC algorithm / GraN-DAG on synthetic data
+    pass
+
+
+def test_interventional_distribution():
+    """Test do-calculus interventions"""
+    # To be implemented
+    # Will test do(X=x) interventions
+    pass
+
+
+def test_transfer_score():
+    """Test transferability score computation"""
+    # To be implemented
+    # Will test edge transferability across domains
+    pass
