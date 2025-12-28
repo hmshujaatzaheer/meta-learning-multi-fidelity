910eba05f23dcd9ff3ca1388b94a1d6846a98027
H M Shujaat Zaheer <50784529+hmshujaatzaheer@users.noreply.github.com>
2025-12-28 04:04:25 -0500

    Update test_fewshot_mf.py

--- tests/test_fewshot_mf.py
+++ tests/test_fewshot_mf.py
@@ -1,1 +1,36 @@
+"""
+Unit tests for FewShotMF
 
+To be implemented during PhD.
+"""
+
+import pytest
+from src.fewshot_mf import FewShotAdapter
+
+
+def test_fewshot_adapter_initialization():
+    """Test few-shot adapter initialization"""
+    adapter = FewShotAdapter(k_shot=5, adaptation_steps=10)
+    assert adapter.k_shot == 5
+    assert adapter.adaptation_steps == 10
+
+
+def test_few_shot_adaptation():
+    """Test adaptation with k examples"""
+    # To be implemented
+    # Will test adaptation with k=5, 10 examples
+    pass
+
+
+def test_support_query_split():
+    """Test support/query set splitting"""
+    # To be implemented
+    # Will test proper data splitting
+    pass
+
+
+def test_pac_bayes_bound():
+    """Test PAC-Bayes performance bounds"""
+    # To be implemented
+    # Will verify theoretical bounds hold
+    pass
